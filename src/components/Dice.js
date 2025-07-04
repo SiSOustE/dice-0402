b5dc254ce0f8b3a82f1f48ee327a55cfabbc8644
Konst <ryzhevsky@gmail.com>
2025-07-04 19:35:11 +0300

    attempt 2

---
+++ src/components/Dice.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './Dice.css';
+
+const Dice = () => {
+  const [diceValue, setDiceValue] = useState(1);
+
+  const rollDice = () => {
+    const newValue = Math.floor(Math.random() * 6) + 1;
+    setDiceValue(newValue);
+  };
+
+  return (
+    <div className="dice-container">
+      <img
+        src={`${process.env.PUBLIC_URL}/images/${diceValue}.svg`}
+        alt={`Dice showing ${diceValue}`}
+        className="dice-image"
+      />
+      <button onClick={rollDice} className="dice-button">
+        Бросить кубик
+      </button>
+    </div>
+  );
+};
+
+export default Dice;
